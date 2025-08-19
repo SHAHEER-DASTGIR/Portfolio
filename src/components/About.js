4725ab8fc051837c593e1fc83c3e0aa019ca7be4
Shaheer Dastgir <105459506+SHAHEER-DASTGIR@users.noreply.github.com>
2025-08-19 18:09:28 +0500

    Updated About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,11 @@ export default function About() {
     <section id="about" className="py-20 px-6 max-w-4xl mx-auto">
       <h2 className="text-3xl font-bold text-blue-400 mb-6">About Me</h2>
       <p className="text-lg leading-relaxed text-gray-300">
-        I’m an Embedded QA Engineer with experience in C, C++, Linux, STM32, 
-        and device drivers. I love solving problems, optimizing low-level code, 
-        and building reliable embedded systems.
+        Electrical Engineer with a strong foundation in software development, embedded systems, and machine learning. I have hands-on experience in Python, Verilog, and UVM-SV with a passion for bridging low-level hardware design and high-level AI systems.
+
+As a Research Assistant at Namal University’s Big Data & AI Center, I contributed to RISC-V CPU design and MIPI core verification using industry-standard tools like Quest Asim and Gtkwave. My work integrates hardware and AI, focusing on innovation and efficiency.
+
+I thrive in collaborative, research-driven environments and am continuously exploring new challenges in embedded systems, AI, and hardware verification.
       </p>
     </section>
   );
